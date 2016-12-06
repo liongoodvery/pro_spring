0efa02063e886c6348e3c302073ddd4806f72a89
lion <lion_good@163.com>
2016-12-06 20:03:25 +0800

    break

--- Chapter3/Chapter3/constructor-injection/src/main/java/com/apress/prospring4/ch3/DeclareSpringComponents.java
+++ Chapter3/Chapter3/constructor-injection/src/main/java/com/apress/prospring4/ch3/DeclareSpringComponents.java
@@ -1,5 +1,6 @@
 package com.apress.prospring4.ch3;
 
+import com.apress.prospring4.ch3.annotation.User;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class DeclareSpringComponents {
@@ -11,6 +12,8 @@ public class DeclareSpringComponents {
         MessageProvider messageProvider = ctx.getBean("messageProvider",
             MessageProvider.class);
 
+        User user = ctx.getBean("user", User.class);
         System.out.println(messageProvider.getMessage());
+        System.out.println(user.getName());
     }
 }
